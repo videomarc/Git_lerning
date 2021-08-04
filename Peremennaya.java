6195861fefbd09b227bceca3ac4a9d362a74748a
videomarc <videoskaz@mail.ru>
2021-08-04 05:53:57 +0300

    Ver 1.0 commit

---
+++ Peremennaya.java
@@ -0,0 +1,11 @@
+ class Peremennaya 
+ {
+  public static void main(String[] args) 
+  {
+      int chislo=3;
+      String moiText="Привет!";
+      System.out.println("Значене переменной chislo:" + chislo);
+      System.out.println("Значение прменной moiText:" + moiText);
+
+  }  
+}
